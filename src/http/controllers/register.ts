325460263180aae0601d1f8bfcfc67e0ce4fbeb9
feh-franc0 <fernandofv1110@gmail.com>
2023-07-22 19:15:11 -0300

    password hash and validation

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
 
@@ -14,13 +15,25 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { address, email, name, number, passwordHash } =
     registerBodySchema.parse(request.body)
 
+  const password_hash = await hash(passwordHash, 6)
+
+  const userWithSameEmail = await prisma.org.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
   await prisma.org.create({
     data: {
       address,
       email,
       name,
       number,
-      password_hash: passwordHash,
+      password_hash,
     },
   })
 
