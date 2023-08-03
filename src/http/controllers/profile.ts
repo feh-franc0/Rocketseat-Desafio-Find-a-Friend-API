8c7f0bff34229688ce7f5b19ed9253225705fcad
feh-franc0 <fernandofv1110@gmail.com>
2023-08-03 01:41:15 -0300

    profile controller

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,17 @@
+import { makeGetOrgProfileUseCase } from '@/use-cases/factories/make-get-org-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const getOrgProfile = makeGetOrgProfileUseCase()
 
-  console.log(request.user.sub)
+  const { org } = await getOrgProfile.execute({
+    orgId: request.user.sub,
+  })
 
-  return reply.status(200).send()
+  return reply.status(200).send({
+    org: {
+      ...org,
+      password_hash: undefined,
+    },
+  })
 }
