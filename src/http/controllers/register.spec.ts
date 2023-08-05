9f54463e47d9c3f4007171ec9bb544f2edfbe0f5
feh-franc0 <fernandofv1110@gmail.com>
2023-08-04 23:47:57 -0300

    E2E test: from register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,24 @@
-import { expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {
-  expect(2 + 2).equal(4)
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/org').send({
+      name: 'John Doe',
+      email: 'johndor@example.com',
+      password: '123456',
+      address: 'rua dos devs',
+      number: '21 9 1020-3040',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
