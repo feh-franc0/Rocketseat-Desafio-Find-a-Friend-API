1ec86e356637660cdc5081f5234b63e404e744b7
feh-franc0 <fernandofv1110@gmail.com>
2023-08-05 11:38:18 -0300

    E2E test: from authenticate

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,5 +1,32 @@
-import { expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {
-  expect(2 + 2).equal(4)
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate  ', async () => {
+    await request(app.server).post('/org').send({
+      name: 'John Doe',
+      email: 'johndor@example.com',
+      password: '123456',
+      address: 'rua dos devs',
+      number: '21 9 1020-3040',
+    })
+
+    const response = await request(app.server).post('/org/sessions').send({
+      email: 'johndor@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
 })
