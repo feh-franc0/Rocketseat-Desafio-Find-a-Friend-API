83f8e6b61ab692645c1aa850865ac221f653d367
feh-franc0 <fernandofv1110@gmail.com>
2023-08-08 16:26:27 -0300

    token refresh strategy

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import fastifyCookie from '@fastify/cookie'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
@@ -9,7 +10,16 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(orgsRoutes)
 app.register(petsRoutes)
